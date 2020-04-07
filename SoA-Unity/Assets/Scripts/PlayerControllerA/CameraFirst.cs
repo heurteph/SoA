fa567df1bb929f6f738ec72625257cae7aba0599
HeuR <pierrehenri.heurte@gmail.com>
2020-04-07 03:06:31 +0200

    [UPDATE] Spherical<->Cartesian automatic conversion

--- SoA-Unity/Assets/Scripts/PlayerControllerA/CameraFirst.cs
+++ SoA-Unity/Assets/Scripts/PlayerControllerA/CameraFirst.cs
@@ -15,28 +15,39 @@ public class CameraFirst : MonoBehaviour
     [SerializeField]
     private GameObject player;
 
+    [SerializeField]
+    [Tooltip("Camera's angular offset from the player's orientation")]
+    private Vector3 cameraAngularOffset;
+
+    [Space]
+    [Header("Position (Spherical Coordinates)")]
     [Space]
 
     [SerializeField]
     [Tooltip("Distance to the player")]
-    [Range(1, 20)]
     private float radius = 1f;
-
-    [SerializeField]
-    [Tooltip("Latitude in degrees")]
-    [Range(-90, 90)]
-    private float latitude = 0f;
+    private float storedRadius;
 
     [SerializeField]
     [Tooltip("Longitude in degrees")]
     [Range(-180, 180)]
     private float longitude = 0f;
+    private float storedLongitude;
 
+    [SerializeField]
+    [Tooltip("Latitude in degrees")]
+    [Range(-90, 90)]
+    private float latitude = 0f;
+    private float storedLatitude;
+
+    [Space]
+    [Header("Position (Cartesian Coordinates)")]
     [Space]
 
     [SerializeField]
-    [Tooltip("Camera's angular offset from the player's orientation")]
-    private Vector3 cameraAngularOffset;
+    [Tooltip("Camera's position in cartesians coordinates relative to the player's position")]
+    private Vector3 cameraOffset;
+    private Vector3 storedCameraOffset;
 
     private void Awake()
     {
@@ -85,6 +96,53 @@ public class CameraFirst : MonoBehaviour
 
         transform.rotation *= Quaternion.Euler(cameraAngularOffset.x, cameraAngularOffset.y, cameraAngularOffset.z);
 
+    }
+
+    /* Spherical-Cartesian conversion functions */
+
+    private void SphericalToCartesian()
+    {
+        cameraOffset.x = radius * Mathf.Cos(longitude * Mathf.Deg2Rad) * Mathf.Cos(latitude * Mathf.Deg2Rad);
+        cameraOffset.y = radius * Mathf.Sin(longitude * Mathf.Deg2Rad) * Mathf.Cos(latitude * Mathf.Deg2Rad);
+        cameraOffset.z = radius * Mathf.Sin(latitude * Mathf.Deg2Rad);
+
+        storedCameraOffset = cameraOffset;
+    }
+
+    public void CartesianToSpherical()
+    {
+        radius = cameraOffset.magnitude;
+        longitude = Mathf.Atan(Mathf.Sqrt(Mathf.Pow(cameraOffset.x,2) + Mathf.Pow(cameraOffset.y,2)) / cameraOffset.z) * Mathf.Rad2Deg;
+        latitude = Mathf.Atan(cameraOffset.x / cameraOffset.y) * Mathf.Rad2Deg;
+
+        storedLatitude = latitude;
+        storedLongitude = longitude;
+        storedRadius = radius;
+    }
+
+    private void OnValidate()
+    {
+        if (storedCameraOffset != cameraOffset)
+        {
+            storedCameraOffset = cameraOffset;
+            CartesianToSpherical();
+        }
+
+        else if (storedRadius != radius)
+        {
+            storedRadius = radius;
+            SphericalToCartesian();
+        }
+        else if (storedLongitude != longitude)
+        {
+            storedLongitude = longitude;
+            SphericalToCartesian();
+        }
+        else if (storedLatitude != latitude)
+        {
+            storedLatitude = latitude;
+            SphericalToCartesian();
+        }
     }
 
 

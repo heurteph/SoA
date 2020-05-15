f13f2dd66f16df1c2a063c057316ec7c9a8f368c
HeuR <pierrehenri.heurte@gmail.com>
2020-05-15 02:35:55 +0200

    [BUGFIX] No more convex mesh errors

---
+++ SoA-Unity/Assets/Scripts/Editor/FindGameObjectWithMesh.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEditor;
+
+public class FindGameObjectsWithMesh : EditorWindow
+{
+    [MenuItem("Tools/ProBuilder/Debug/Find GameObjects with Mesh Name")]
+    private static void MenuInit()
+    {
+        GetWindow<FindGameObjectsWithMesh>(true, "Find GameObjects with Name", true);
+    }
+
+    private string m_MeshName = "";
+    private MeshFilter[] m_Matches = new MeshFilter[0];
+
+    private void OnGUI()
+    {
+        m_MeshName = EditorGUILayout.TextField("Mesh", m_MeshName);
+
+        if (GUILayout.Button("Find"))
+            m_Matches = Resources.FindObjectsOfTypeAll<MeshFilter>().Where(
+                x => x.sharedMesh != null && x.sharedMesh.name.Contains(m_MeshName))
+                    .ToArray();
+
+        GUILayout.Label("GameObjects with Mesh Name", EditorStyles.boldLabel);
+
+        foreach (MeshFilter mf in m_Matches)
+        {
+            if (GUILayout.Button(mf.sharedMesh.name))
+                EditorGUIUtility.PingObject(mf);
+        }
+    }
+}

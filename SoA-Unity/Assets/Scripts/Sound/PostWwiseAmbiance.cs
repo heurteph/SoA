ecd8504d7713b736bb28b84867f46c97edd9f001
HeuR <pierrehenri.heurte@gmail.com>
2020-05-27 23:05:06 +0200

    [ADD] Trigger script for ambiance switch

---
+++ SoA-Unity/Assets/Scripts/Sound/PostWwiseAmbiance.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AK.Wwise;
+
+public class PostWwiseAmbiance : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("The event of the park ambiance")]
+    private AK.Wwise.Event parkAmbianceEvent;
+    public AK.Wwise.Event ParkAmbianceEvent { get { return parkAmbianceEvent; } }
+
+    [SerializeField]
+    [Tooltip("The event of the city ambiance")]
+    private AK.Wwise.Event cityAmbianceEvent;
+    public AK.Wwise.Event CityAmbianceEvent { get { return cityAmbianceEvent; } }
+
+    [SerializeField]
+    [Tooltip("The ambiance the game starts with")]
+    private AMBIANCE ambiance = AMBIANCE.NONE;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(parkAmbianceEvent == null)
+        {
+            throw new System.NullReferenceException("No parkAmbiance sound event set on the script PostWwiseAmbiance");
+        }
+        if (cityAmbianceEvent == null)
+        {
+            throw new System.NullReferenceException("No cityAmbiance sound event set on the script PostWwiseAmbiance");
+        }
+
+        switch (ambiance)
+        {
+            case AMBIANCE.PARK:
+                CityAmbianceEvent.Stop(gameObject);
+                ParkAmbianceEvent.Post(gameObject);
+                break;
+            case AMBIANCE.CITY:
+                ParkAmbianceEvent.Stop(gameObject);
+                CityAmbianceEvent.Post(gameObject);
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ SoA-Unity/Assets/Scripts/Sound/TriggerAmbiance.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum AMBIANCE { PARK, CITY, NONE };
+
+public class TriggerAmbiance : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("The type of ambiance that is triggered when crossing this collider")]
+    private AMBIANCE ambiance = AMBIANCE.NONE;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if(other.GetComponent<PostWwiseAmbiance>() == null)
+            {
+                throw new System.NullReferenceException("No PostWwiseAmbiance script set on the player");
+            }
+
+            if (ambiance == AMBIANCE.PARK)
+            {
+                other.GetComponent<PostWwiseAmbiance>().CityAmbianceEvent.Stop(other.gameObject);
+                other.GetComponent<PostWwiseAmbiance>().ParkAmbianceEvent.Post(other.gameObject);
+            }
+            else if (ambiance == AMBIANCE.CITY)
+            {
+                other.GetComponent<PostWwiseAmbiance>().ParkAmbianceEvent.Stop(other.gameObject);
+                other.GetComponent<PostWwiseAmbiance>().CityAmbianceEvent.Post(other.gameObject);
+            }
+        }
+    }
+}

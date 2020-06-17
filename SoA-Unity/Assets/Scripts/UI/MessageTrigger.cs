4455fc562a0017de490cc88150e363fe8caa1bdf
HeuR <pierrehenri.heurte@gmail.com>
2020-06-17 12:44:11 +0200

    [ADD] Restaurant terrace + Message System

---
+++ SoA-Unity/Assets/Scripts/UI/MessageTrigger.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MessageTrigger : MonoBehaviour
+{
+    private GameObject messageManager;
+
+    [SerializeField]
+    [Tooltip("Tutorial Message to be displayed")]
+    private string infoMessage;
+
+    private bool fired;
+
+    private void Awake()
+    {
+        messageManager = transform.parent.gameObject;
+        fired = false;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!fired)
+        {
+            if (other.CompareTag("Player"))
+            {
+                messageManager.GetComponent<MessageManager>().DisplayMessage(infoMessage);
+                fired = true;
+            }
+        }
+    }
+}

3abd83d4fea76fae6f9a37891ce346e13a0c148a
HeuR <pierrehenri.heurte@gmail.com>
2020-06-20 23:31:45 +0200

    [UPDATE] Stretching script

--- SoA-Unity/Assets/Scripts/Menus/FitBackground.cs
+++ SoA-Unity/Assets/Scripts/Menus/FitBackground.cs
@@ -5,21 +5,34 @@ using UnityEngine.UI;
 
 public class FitBackground : MonoBehaviour
 {
-    private Image background;
     void Awake()
     {
-        background = GetComponent<Image>();
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Image background = GetComponent<Image>();
         if (background == null) return;
 
         background.SetNativeSize();
         transform.localScale = new Vector3(1, 1, 1);
         float width = background.rectTransform.rect.size.x;
         float height = background.rectTransform.rect.size.y;
+
+        // Set the anchor at the center of the screen and center the image on it
         background.rectTransform.anchorMin = new Vector2(0.5f, 0.5f);
         background.rectTransform.anchorMax = new Vector2(0.5f, 0.5f);
         background.rectTransform.anchoredPosition = Vector2.zero;
 
-
+        // Check if the screen's ratio is higher or wider than the image's one
         if ((float)Screen.width / Screen.height > width / height)
         {
             // then match screen's width
@@ -32,17 +45,5 @@ public class FitBackground : MonoBehaviour
             background.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, Screen.height);
             background.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, width * Screen.height / height);
         }
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }

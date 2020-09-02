3bf48c68d8bc0d0c7b3a16d40937654eca7ea2ed
HeuR <pierrehenri.heurte@gmail.com>
2020-09-02 17:08:08 +0200

    [ADD] Json deserialization + Cutscene manager

---
+++ SoA-Unity/Assets/Scripts/Cutscene/CutsceneManager.cs
@@ -0,0 +1,174 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+namespace story
+{
+    [System.Serializable]
+    public class Story
+    {
+        public List<Scene> scenes;
+
+        [System.Serializable]
+        public class Scene
+        {
+            public List<Action> actions;
+
+            [System.Serializable]
+            public class Action
+            {
+                public string type;
+                public string metadata;
+                public string data;
+            };
+        };
+
+        public Story(string text)
+        {
+            Deserialize(text);
+        }
+        public void Deserialize(string text)
+        {
+            JsonUtility.FromJsonOverwrite(text, this);
+        }
+    }
+
+
+    public class CutsceneManager : MonoBehaviour
+    {
+        [SerializeField]
+        [Tooltip("Story in JSON format")]
+        private TextAsset jsonData;
+
+        [SerializeField]
+        [Tooltip("Story deserialized")]
+        private Story story;
+
+        private List<Story.Scene>.Enumerator itScenes;
+        private List<Story.Scene.Action>.Enumerator itActions;
+
+        [Space]
+
+        // TO DO : Use dictionaries or hashmaps instead of list to find them easily
+
+        [SerializeField]
+        [Tooltip("List of the images")]
+        private Image[] images;
+
+        [SerializeField]
+        [Tooltip("List of the sounds")]
+        private AkEvent[] sounds;
+
+        [Space]
+
+        [SerializeField]
+        [Tooltip("The message box")]
+        private TextMeshProUGUI messageBox;
+
+        [SerializeField]
+        [Tooltip("The header of the message box")]
+        private Text nameTag;
+
+        public delegate void ReadyHandler(string text);
+        public event ReadyHandler NewMessageEvent;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            // Callback functions
+            NewMessageEvent += messageBox.GetComponent<TextRevealEffect>().WriteMessage;
+            messageBox.GetComponent<TextRevealEffect>().MessageShownEvent += ExecuteNextAction;
+
+            // TO DO : Runtime loading of the json file for multilanguage support (EN/FR)
+            //jsonData = Resources.Load<TextAsset>("Cutscenes/cutscenes");
+
+            story = new Story(jsonData.text);
+            itScenes = story.scenes.GetEnumerator();
+            itScenes.MoveNext(); // Initialization ?
+            itActions = itScenes.Current.actions.GetEnumerator();
+
+            ExecuteNextAction();
+            //StartCoroutine("PlayAllCutscene");
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void ExecuteNextAction()
+        {
+            ExecuteAction(GetNextAction());
+        }
+
+        public Story.Scene.Action GetNextAction()
+        {
+            if (!itActions.MoveNext())
+            {
+                if (!itScenes.MoveNext())
+                {
+                    Debug.Log("Fin de la cutscene");
+                    // Close cutscene and start game
+                    return null;
+                }
+                Debug.Log("Scène suivante");
+                itActions = itScenes.Current.actions.GetEnumerator();
+                itActions.MoveNext(); // Initialization
+            }
+            Debug.Log("Action suivante");
+            return itActions.Current;
+        }
+
+        void ExecuteAction(Story.Scene.Action action)
+        {
+            if (action == null) return;
+
+            switch (action.type)
+            {
+                case "message":
+
+                    nameTag.text = action.metadata;
+                    //messageBox.text = action.data;
+                    NewMessageEvent(action.data);
+                    break;
+
+                case "sound":
+
+                    // TO DO : Play a sound
+                    ExecuteNextAction(); // skip for now
+                    break;
+
+                case "image":
+
+                    // TO DO : Display an image
+                    ExecuteNextAction(); // skip for now
+                    break;
+
+                default:
+
+                    // TO DO : Skip action
+                    break;
+            }
+        }
+
+        /*
+        IEnumerator PlayAllCutscene()
+        {
+            foreach (var scene in story.scenes)
+            {
+                foreach (var action in scene.actions)
+                {
+                    Debug.Log("event : " + action.type + ", " + action.metadata + ", " + action.data);
+
+                    ExecuteAction(action);
+
+                    yield return new WaitForSeconds(0.5f);
+                }
+            }
+        }
+        */
+    }
+}
---
+++ SoA-Unity/Assets/Scripts/Cutscene/TextRevealEffect.cs
@@ -0,0 +1,197 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using story;
+
+public class TextRevealEffect : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("The cutscene manager")]
+    private GameObject cutsceneManager;
+
+    public delegate void DisplayHandler();
+    public event DisplayHandler MessageShownEvent;
+
+    //private string text;
+
+    public delegate IEnumerator DisplayMessage(string text);
+    DisplayMessage del;
+
+    [Space]
+    [Header("Message Animation Options")]
+
+    [SerializeField]
+    [Tooltip("The delay between two letters")]
+    [Range(1,5)]
+    private float messageSpeed = 1;
+
+    [SerializeField]
+    [Tooltip("The number of characters appearing at the same time")]
+    [Range(1, 10)]
+    private float rolloverCharacterSpread = 5;
+
+    [SerializeField]
+    [Tooltip("How long a message remains in seconds")]
+    [Range(0.5f, 5)]
+    private float messageDuration = 1.5f;
+
+    private TextMeshProUGUI textMesh;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        textMesh = GetComponent<TextMeshProUGUI>();
+        if (textMesh == null)
+        {
+            throw new System.NullReferenceException("No TextMeshProUGUI attached to " + transform.name);
+        }
+
+        textMesh.text = string.Empty;
+        
+        del = new DisplayMessage(RevealFade);
+    }
+
+    public void WriteMessage(string text /*, callback */)
+    {
+        // Possible runtime change of the display method
+        // DisplayMessage del = new DisplayMessage(callback);
+        StartCoroutine(del(text));
+    }
+
+    IEnumerator RevealLetterByLetter(string text)
+    {
+        textMesh.text = text;
+        textMesh.maxVisibleCharacters = 0;
+
+        float delay;
+        textMesh.ForceMeshUpdate(); // update textInfo to get actual characterCount
+        for (int i = 0; i <= textMesh.textInfo.characterCount; i++)
+        {
+            textMesh.maxVisibleCharacters = i;
+            delay = 0.1f / (2 * messageSpeed);
+            yield return new WaitForSeconds(delay);
+        }
+
+        yield return new WaitForSeconds(messageDuration);
+
+        MessageShownEvent();
+    }
+
+    IEnumerator RevealWordByWord(string text)
+    {
+        textMesh.text = text;
+        textMesh.maxVisibleWords = 0;
+
+        float delay;
+        textMesh.ForceMeshUpdate(); // update textInfo to get actual wordCount
+        for (int i = 0; i <= textMesh.textInfo.wordCount + 1; i++)
+        {
+            textMesh.maxVisibleWords = i;
+            delay = 0.1f / (2 * messageSpeed);
+            yield return new WaitForSeconds(delay);
+        }
+
+        yield return new WaitForSeconds(messageDuration);
+
+        MessageShownEvent();
+    }
+
+    IEnumerator RevealFade(string text)
+    {
+        textMesh.text = text;
+        textMesh.ForceMeshUpdate();
+
+        TMP_TextInfo textInfo = textMesh.textInfo;
+        Color32[] newVertexColors;
+
+        int currentCharacterIndex = 0;
+        int startingCharacterIndex = currentCharacterIndex;
+        bool isRangeMax = false;
+
+        while (!isRangeMax)
+        {
+            int characterCount = textInfo.characterCount;
+
+            // Spread should not exceed the number of characters.
+            //float RolloverCharacterSpread = 5; // how many character appearing at once
+            byte fadeSteps = (byte)Mathf.Max(1, 255 / rolloverCharacterSpread);
+
+            //Debug.Log("begin : " + startingCharacterIndex + " & end : " + currentCharacterIndex);
+
+            for (int i = startingCharacterIndex; i < currentCharacterIndex + 1; i++)
+            {
+                // Skip characters that are not visible
+                if (!textInfo.characterInfo[i].isVisible) continue;
+
+                // Get the index of the material used by the current character.
+                int materialIndex = textInfo.characterInfo[i].materialReferenceIndex;
+
+                // Get the vertex colors of the mesh used by this text element (character or sprite).
+                newVertexColors = textInfo.meshInfo[materialIndex].colors32;
+
+                // Get the index of the first vertex used by this text element.
+                int vertexIndex = textInfo.characterInfo[i].vertexIndex;
+
+                // Get the current character's alpha value.
+                byte alpha = (byte)Mathf.Clamp(newVertexColors[vertexIndex + 0].a + fadeSteps, 0, 255);
+
+                Debug.Log("alpha de " + text[i].ToString() + " : " + alpha);
+
+                // Set new alpha values.
+                newVertexColors[vertexIndex + 0].a = alpha;
+                newVertexColors[vertexIndex + 1].a = alpha;
+                newVertexColors[vertexIndex + 2].a = alpha;
+                newVertexColors[vertexIndex + 3].a = alpha;
+
+                // Tint vertex colors
+                // Note: Vertex colors are Color32 so we need to cast to Color to multiply with tint which is Color.
+                //newVertexColors[vertexIndex + 0] = (Color)newVertexColors[vertexIndex + 0] * ColorTint;
+                //newVertexColors[vertexIndex + 1] = (Color)newVertexColors[vertexIndex + 1] * ColorTint;
+                //newVertexColors[vertexIndex + 2] = (Color)newVertexColors[vertexIndex + 2] * ColorTint;
+                //newVertexColors[vertexIndex + 3] = (Color)newVertexColors[vertexIndex + 3] * ColorTint;
+
+                if (alpha == 255)
+                {
+                    startingCharacterIndex += 1;
+
+                    if (startingCharacterIndex == characterCount)
+                    {
+                        isRangeMax = true; // Would end the coroutine.
+
+                        /*
+                        
+                        // Update mesh vertex data one last time.
+                        textMesh.UpdateVertexData(TMP_VertexDataUpdateFlags.Colors32);
+
+                        yield return new WaitForSeconds(1.0f);
+
+                        // Reset the text object back to original state.
+                        textMesh.ForceMeshUpdate();
+
+                        yield return new WaitForSeconds(1.0f);
+
+                        // Reset our counters.
+                        currentCharacterIndex = 0;
+                        startingCharacterIndex = 0;
+
+                        */
+                    }
+                }
+            }
+
+            // Upload the changed vertex colors to the Mesh.
+            textMesh.UpdateVertexData(TMP_VertexDataUpdateFlags.Colors32);
+
+            if (currentCharacterIndex + 1 < characterCount) currentCharacterIndex += 1;
+
+            yield return new WaitForSeconds(0.01f / messageSpeed);
+        }
+
+        yield return new WaitForSeconds(messageDuration);
+        textMesh.alpha = 0;
+
+        MessageShownEvent();
+    }
+}
